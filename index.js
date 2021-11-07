ac4d33fef19ed5c902a1f14479272f18af7d6961
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-07 15:56:19 -0800

    3.3

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ app.get('/api/info',(req,res) =>{
   res.send(`<p>phonebook has info for ${persons.length} people </p> <p>${new Date()}</p>`)
 })
 
+//3.2 - Fetching a single resource
+//the url directs the functionality
+//here the route indicates fetching a single resource based off id
+//we take the id by looking at the parameters of the request (more details on the express routing page)
+//this id is a string so we make it a numberr, and compare it to all the person objects in the person array and return the matching person
+
+app.get('/api/persons/:id',(req,res) =>{
+  const id = req.params.id
+  const person = persons.find(person=>person.id === Number(id))
+  if (person) res.json(person)
+  else res.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
