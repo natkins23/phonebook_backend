197b33bfb19ac50a32579bae14d169d1d7d0f224
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-07 20:33:47 -0800

    3.43_done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 
 let persons = [
   { 
@@ -36,12 +38,31 @@ app.get('/api/info',(req,res) =>{
   res.send(`<p>phonebook has info for ${persons.length} people </p> <p>${new Date()}</p>`)
 })
 
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+  persons.push(person)
+  response.json(person)
+})
+
+
+
+//3.4 - delete a resource
+//Note: you need the / before api - too tired to notice i misspelled api...
+app.delete('/api/persons/:id', (req, res)=>{
+  const id = Number(req.params.id)
+  persons = persons.filter(p=>p.id === id)
+  res.status(204).end()
+
+})
+
+
+
 //3.3 - Fetching a single resource
 //the url directs the functionality
 //here the route indicates fetching a single resource based off id
 //we take the id by looking at the parameters of the request (more details on the express routing page)
 //this id is a string so we make it a numberr, and compare it to all the person objects in the person array and return the matching person
-
 app.get('/api/persons/:id',(req,res) =>{
   const id = req.params.id
   const person = persons.find(person=>person.id === Number(id))
