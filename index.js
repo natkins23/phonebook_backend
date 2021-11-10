620ccd724929322a7453c9ec879fd29278b9571c
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-10 12:21:43 -0800

    3.7_test

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let persons = [
   { 
@@ -90,6 +99,14 @@ app.get('/api/persons/:id',(req,res) =>{
   if (person) res.json(person)
   else res.status(404).end()
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 
 const PORT = 3001
 app.listen(PORT)
