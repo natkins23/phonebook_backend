8daa3167ee87c662b3e4979a4e4267d3a195766b
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-10 12:31:03 -0800

    3.7_test

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 app.use(express.json())
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
 
-app.use(requestLogger)
+app.use(morgan('tiny'))
 
 let persons = [
   { 
