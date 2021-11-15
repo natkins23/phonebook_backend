4e0ce69f5a88e4244f525ec9c4e76ff0370407b0
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 17:16:29 -0800

    automated commits dsdsd

--- src/branch.js
+++ src/branch.js
@@ -5,26 +5,26 @@ let msg = args.slice(2).join(' ')
 console.log(msg)
 exec('git add .', addcb)
 
-// function addcb(err, stdout,stderr){
-//     if (err){
-//         console.log(err)
-//         return;
-//     }
-// exec(`git commit -m \"${msg}\"`, commitcb)
-// }
+function addcb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return;
+    }
+exec(`git commit -m \"${msg}\"`, commitcb)
+}
 
-// function commitcb(err, stdout,stderr){
-//     if (err){
-//         console.log(err)
-//         return;
-//     }
-//     exec(`git push`, pushcb)
+function commitcb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return;
+    }
+    exec(`git push`, pushcb)
 
-// }
-// function pushcb(err, stdout,stderr){
-//     if (err){
-//         console.log(err)
-//         return;
-//     }
-//     console.log('successful commit')
-// }
+}
+function pushcb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return;
+    }
+    console.log('successful commit')
+}
--- src/push.js
+++ src/push.js
@@ -1,17 +1,30 @@
 const {exec} = require('child_process')
-const { stdout, stderr } = require('process')
 let args = process.argv
 
 let msg = args.slice(2).join(' ')
+console.log(msg)
+exec('git add .', addcb)
 
-exec('git add .', cb)
-exec(`git commit -m \"${msg}\"`, cb)
-exec(`git push`, cb)
-
-
-function cb(err, stdout,stderr){
+function addcb(err, stdout,stderr){
     if (err){
         console.log(err)
-        return
+        return;
     }
+exec(`git commit -m \"${msg}\"`, commitcb)
+}
+
+function commitcb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return;
+    }
+    exec(`git push`, pushcb)
+
+}
+function pushcb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return;
+    }
+    console.log('successful commit')
 }
