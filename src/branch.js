ab6767d44bc16f717132f841f574e78df3712183
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 17:11:08 -0800

    automated commits dsdsd

--- src/branch.js
+++ src/branch.js
@@ -1,26 +1,33 @@
 const {exec} = require('child_process')
-const {stdout,stderr} = require('process')
 let args = process.argv
 
 let msg = args.slice(2).join(' ')
 
 exec('git add .', cb)
-try{
-    exec(`git commit -m \"${msg}\"`, cb)
-}
-    catch (error){
-        console.log('this is a test',error)
-    }
-exec(`git push`, cb)
 
 function test(err, stdout,stderr){
     console.log('test')
 }
 
-function cb(err, stdout,stderr){
+function add_cb(err, stdout,stderr){
     if (err){
-        // console.log('this is a test',err)
-        // exec('git status',test)
+        console.log(err)
         return
     }
+exec(`git commit -m \"${msg}\"`, cb)
+}
+function commit_cb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return
+    }
+    exec(`git push`, cb)
+
+}
+function push_cb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return
+    }
+    console.log('successful commit')
 }
