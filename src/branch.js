6ee26d16df964d199bac41b97ffd0de23c4bfaa0
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 17:00:34 -0800

    automated commits dsdsd

--- src/branch.js
+++ src/branch.js
@@ -7,11 +7,14 @@ exec('git add .', cb)
 exec(`git commit -m \"${msg}\"`, cb)
 exec(`git push`, cb)
 
+function test(err, stdout,stderr){
+    console.log('test')
+}
 
 function cb(err, stdout,stderr){
     if (err){
         console.log(err)
-        exec('git status')
+        exec('git status',test)
         return
     }
 }
