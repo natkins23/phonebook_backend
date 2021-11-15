0ebc267f38a2db2c36167c24d78fed53597801ab
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 16:52:23 -0800

    automated commits dsdsd

---
+++ src/branch.js
@@ -0,0 +1,16 @@
+const {exec} = require('child_process')
+let args = process.argv
+
+let msg = args.slice(2).join(' ')
+
+exec('git add .', cb)
+exec(`git commit -m \"${msg}\"`, cb)
+exec(`git push`, cb)
+
+
+function cb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return
+    }
+}
