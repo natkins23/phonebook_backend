899034c9d1d7b9471b2be9769f2f993ddeca0471
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 17:15:04 -0800

    automated commits dsdsd

--- src/branch.js
+++ src/branch.js
@@ -2,30 +2,29 @@ const {exec} = require('child_process')
 let args = process.argv
 
 let msg = args.slice(2).join(' ')
-
 console.log(msg)
 exec('git add .', addcb)
 
-function addcb(err, stdout,stderr){
-    if (err){
-        console.log(err)
-        return;
-    }
-exec(`git commit -m \"${msg}\"`, commitcb)
-}
+// function addcb(err, stdout,stderr){
+//     if (err){
+//         console.log(err)
+//         return;
+//     }
+// exec(`git commit -m \"${msg}\"`, commitcb)
+// }
 
-function commitcb(err, stdout,stderr){
-    if (err){
-        console.log(err)
-        return;
-    }
-    exec(`git push`, pushcb)
+// function commitcb(err, stdout,stderr){
+//     if (err){
+//         console.log(err)
+//         return;
+//     }
+//     exec(`git push`, pushcb)
 
-}
-function pushcb(err, stdout,stderr){
-    if (err){
-        console.log(err)
-        return;
-    }
-    console.log('successful commit')
-}
+// }
+// function pushcb(err, stdout,stderr){
+//     if (err){
+//         console.log(err)
+//         return;
+//     }
+//     console.log('successful commit')
+// }
