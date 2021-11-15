ae7ab27afb07459cc2e853fbc5b4d4f898a2f4a4
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-14 16:28:14 -0800

    automated,commits

---
+++ src/push.js
@@ -0,0 +1,19 @@
+const {exec} = require('child_process')
+const { stdout, stderr } = require('process')
+let args = process.argv
+
+let msg = args.slice(2)
+
+exec('git add .', cb)
+exec(`git commit -m ${msg}`, cb)
+exec(`git push`, cb)
+
+
+function cb(err, stdout,stderr){
+    if (err){
+        console.log(err)
+        return
+    }
+}
+
+console.log(msg.join(' '))
