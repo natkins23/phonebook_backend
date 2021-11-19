3bd8a2c303508f92113bb0e90de0f605f4450ff0
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-18 16:56:22 -0800

    Exercise 3.17 completed: added put request

--- index.js
+++ index.js
@@ -10,65 +10,57 @@ const { json } = require('express')
 const app = express()
 
 //3.8 - morgan - logger middleware
-morgan.token('body', req => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-  return maxId + 1
-}
-
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 //3.13 - updated to recieve persons from mongodb server
-app.get('/api/persons',(req,res) =>{
-  Person.find({}).then(persons=>{
-  res.json(persons)
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
-})
-
 
 //depricated - removed local persons object
 // app.get('/api/info',(req,res) =>{
 //   res.send(`<p>phonebook has info for ${persons.length} people </p> <p>${new Date()}</p>`)
 // })
 
-
-app.get('/api/persons/:id',(req,res) =>{
+app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  const person = persons.find(person=>person.id === Number(id))
+  const person = persons.find((person) => person.id === Number(id))
   if (person) res.json(person)
   else res.status(404).end()
 })
 
-
 //3.5 - Post request - adding entries - increment ids
 //3.5 post - deprecated
-// app.post('/api/persons', (req, res) => { 
+// app.post('/api/persons', (req, res) => {
 //   const person = req.body
 //   const nameExists = persons.find(p=>p.name === person.name)
 
 //   ////3.6 -error handling
 //   if(!person.name){
-//     return res.status(400).json({ 
-//       error: 'name missing' 
+//     return res.status(400).json({
+//       error: 'name missing'
 //     })
 //   }
 //   else if  (!person.number){
-//     return respresonse.status(400).json({ 
-//       error: 'name missing' 
+//     return respresonse.status(400).json({
+//       error: 'name missing'
 //     })
 //   }else if  (nameExists){
-//     return res.status(400).json({ 
-//       error: 'name exists' 
+//     return res.status(400).json({
+//       error: 'name exists'
 //     })
 //   }
 //   const newPerson = {
@@ -82,32 +74,45 @@ app.get('/api/persons/:id',(req,res) =>{
 // })//end of post
 
 //3.14 post req - using MongoDB server
-app.post('/api/persons', (req, res) => { 
+app.post('/api/persons', (req, res) => {
   const body = req.body
   if (body.name === undefined) {
     return response.status(400).json({ error: 'name missing' })
-  }else if (body.number === undefined) {
+  } else if (body.number === undefined) {
     return response.status(400).json({ error: 'number missing' })
   }
   const person = new Person({
     name: req.body.name,
-    number: req.body.number
-})
-  person.save().then(savedPerson=>{
-    res.json(savedPerson);
+    number: req.body.number,
+  })
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
   })
 })
 
 //3.15 - delete using mongoDB schema
-app.delete('/api/persons/:id', (req, res, next)=>{
-  Person.findByIdAndRemove(req.params.id).then(result=>{
-    res.status(204).end()
-  }).catch(error=>{
-    //using 3.16 errorHandling middleware
-    next(error)
-  })
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => {
+      //using 3.16 errorHandling middleware
+      next(error)
+    })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  person = { name: body.name, number: body.number }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((newPerson) => {
+      res.json(newPerson)
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -115,13 +120,12 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-
 //3.16 -error handler middleware
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
-  if (error.message === 'CastError'){
-    return response.status(400).send({error:'malformatted id'})
+  if (error.message === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 }
 app.use(errorHandler)
