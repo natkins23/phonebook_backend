86898658103988b6370b30f8d7be095f3a930757
Nathan Watkins <nathancwatkins23@gmail.com>
2021-11-23 12:40:34 -0800

    update

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ app.put('/api/persons/:id', (req, res, next) => {
     .catch((error) => next(error))
 })
 
+
+.catch(error => {
+  // 3.20 this is the way to access the error message
+  notifyWith(`${error.response.data.error} `, 'error')
+})
+
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
